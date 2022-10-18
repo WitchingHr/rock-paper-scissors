e4fbc4a584dcfb82fede9cbdb311acf3a92e9c00
WitchingHr <matt1494@gmail.com>
2022-10-17 23:41:03 -0400

    Write player choice fn

--- script.js
+++ script.js
@@ -13,7 +13,31 @@ function getComputerChoice() {
 console.log(`Computer picks: ${getComputerChoice()}`);
 
 function playerChoice() {
-    return prompt('Type your weapon', 'Rock, Paper or Scissors')
+    let x = prompt('Type your weapon', 'Rock, Paper or Scissors');
+
+    switch (x) {
+        case 'Rock':
+        case 'rock':
+        case 'ROCK':
+            console.log('Player picks: Rock')
+            return 'Rock';
+            break;
+        case 'Paper':
+        case 'paper':
+        case 'PAPER':
+            console.log('Player picks: Paper')
+            return 'Paper';
+            break;
+        case 'Scissors':
+        case 'scissors':
+        case 'SCISSORS':
+            console.log('Player picks: Scissors')
+            return 'Scissors';
+            break;
+        default:
+            alert('Choice not recognized. Choose again');
+            playerChoice();
+    }
 }
 
-console.log(playerChoice());
+playerChoice();

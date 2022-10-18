0c9c4d23484bdd05800621a2e7c0cbd9b35f6f86
WitchingHr <matt1494@gmail.com>
2022-10-18 13:32:56 -0400

    Add comparison function

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
 
     if (x === 0) {
+        console.log('Computer picks: Rock')
         return 'Rock'
     } else if (x === 1) {
+        console.log('Computer picks: Paper')
         return 'Paper'
     } else {
-        return 'Scissor'
+        console.log('Computer picks: Scissors')
+        return 'Scissors'
     }
 }
-
-console.log(`Computer picks: ${getComputerChoice()}`);
 
 function playerChoice() {
     let x = prompt('Type your weapon', 'Rock, Paper or Scissors');
@@ -35,4 +36,58 @@ function playerChoice() {
     }
 }
 
-playerChoice();
+let playerWeapon = playerChoice();
+let compWeapon = getComputerChoice();
+
+//Comparison:
+
+let playWeight = 1;
+let compWeight;
+
+if (playerWeapon === 'Rock') {
+
+    if (compWeapon === 'Rock') {
+        compWeight = 1;
+    } else if (compWeapon === 'Paper') {
+        compWeight = 2;
+    } else {
+        compWeight = 0;
+    }
+
+} else if (playerWeapon === 'Paper') {
+
+    if (compWeapon === 'Rock') {
+        compWeight = 0;
+    } else if (compWeapon === 'Paper') {
+        compWeight = 1;
+    } else {
+        compWeight = 2;
+    }
+
+} else {
+
+    if (compWeapon === 'Rock') {
+        compWeight = 2;
+    } else if (compWeapon === 'Paper') {
+        compWeight = 0;
+    } else {
+        compWeight = 1;
+    }
+
+}
+
+console.log(compWeight);
+
+// for loop: game repeats until counter = 5
+
+//Score counter
+//if player beats computer,
+//add 1 to player score
+//if computer beats player,
+//add 1 to computer score
+
+//Win condition
+//if player counter = 5,
+//player wins
+//if computer counter = 5,
+//computer wins
